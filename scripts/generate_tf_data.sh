ec5cd4400101c8366cd995e0247d38bcb835a6c7
Tobias Watzel <tobias.watzel@tum.de>
2018-11-09 12:02:48 +0100

    Reworked generate_tf_data.sh script for creating train/dev/test data for tensorflow

--- scripts/generate_tf_data.sh
+++ scripts/generate_tf_data.sh
@@ -17,7 +17,7 @@ nj=20   # number of jobs for alignments
 njd=30  # number of jobs for decoding
 cmd=run.pl
 stage=-5
-state_based=true
+state_based=false
 train_data=true
 mono=true
 
@@ -59,14 +59,15 @@ for f in $source_dir/tree $source_dir/final.mdl $own_model_dir/tree $own_model_d
 done
 
 if [[ "$state_based" == "true" ]]; then
-    string="-pdf"
+    echo "---Creating labels for states---"
+    string="pdf"
 else
-    string="-phones"
+    echo "---Creating labels for phones---"
+    string="phones"
 fi
 
 # define own model path
 mdl=
-
 
 if [ $stage -le -4 ]; then
     # use the dev/test lattices to get the alignments for dev and test
@@ -81,7 +82,7 @@ fi
 if [ $stage -le -3 ]; then
 # convert alignments to own model
     if [[ "$mono" == "true" ]]; then
-        echo "---convert alignments to monophone model---"
+        echo "---Convert alignments to monophone model---"
         for dataset in train test dev; do
             if [[ "$dataset" == "train" ]]; then
                 $cmd JOB=1:$nj $working_dir/log/get_model_alignments_${dataset}_JOB convert-ali $source_dir/final.mdl $own_model_dir/final.mdl $own_model_dir/tree\
@@ -98,7 +99,7 @@ if [ $stage -le -3 ]; then
         #     "ark,t:gunzip -c $source_dir/ali.JOB.gz|" "ark,t:|gzip -c >$working_dir/ali_train.JOB.gz"
         # mdl=$own_model_dir/final.mdl
     else
-        echo "---copy alignments of triphone model---"
+        echo "---Copy alignments of triphone model---"
         # else copy alignments and rename them from triphone model
         cp $source_dir/ali.*.gz $working_dir
         rename 's/ali/ali_train/' $working_dir/ali.*
@@ -108,66 +109,18 @@ if [ $stage -le -3 ]; then
 fi
 
 
-# ali_dir=    # setting ali_dir var
-# # convert alignments to pdf based or phoneme based
-# if [ $stage -le -2 ]; then
-#     if [[ "$state_based" == "true" ]]; then
-#         echo "---map the alignments to $string---"
-#         # get state based alignments
-#         for dataset in train test dev; do
-#             if [[ "$dataset" == "train" ]]; then
-#                 $cmd JOB=1:$nj $working_dir/log/state_based_alignments_${dataset}_JOB ali-to-pdf $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|"\
-#                     "ark,t:|gzip -c >$working_dir/ali_pdf_${dataset}.JOB.gz"
-#             else
-#                 $cmd JOB=1:$njd $working_dir/log/state_based_alignments_${dataset}_JOB ali-to-pdf $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|"\
-#                     "ark,t:|gzip -c >$working_dir/ali_pdf_${dataset}.JOB.gz"
-#             fi
-#             gunzip -c $working_dir/ali_pdf_${dataset}.*> $working_dir/alignments/all_ali_pdf_${dataset}
-#             # ali_dir_${dataset}="$working_dir/alignments/all_ali_pdf_${dataset}"
-#             # remove old files
-#             # rm $working_dir/ali_* 2>/dev/null
-#         done
-#
-#         # # create one big alignment file
-#         # gunzip -c $working_dir/ali_pdf.*> $working_dir/alignments/all_ali_pdf
-#         # ali_dir=$working_dir/alignments/all_ali_pdf
-#         # # remove old files
-#         # rm $working_dir/ali_pdf.* 2>/dev/null
-#     else
-#         echo "---map the alignments to $string---"
-#         # get phoneme based alignments
-#         for dataset in train test dev; do
-#             if [[ "$dataset" == "train" ]]; then
-#                 $cmd JOB=1:$nj $working_dir/log/phoneme_based_alignments_${dataset}_JOB ali-to-phones --per-frame $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|"\
-#                     "ark,t:|gzip -c >$working_dir/ali_pho_${dataset}.JOB.gz"
-#             else
-#                 $cmd JOB=1:$njd $working_dir/log/phoneme_based_alignments_${dataset}_JOB ali-to-phones --per-frame $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|"\
-#                     "ark,t:|gzip -c >$working_dir/ali_pho_${dataset}.JOB.gz"
-#             fi
-#             # create one big alignment file
-#             gunzip -c $working_dir/ali_pho_${dataset}.*> $working_dir/alignments/all_ali_phoneme_${dataset}
-#         done
-#
-#         # remove old files
-#         # rm $working_dir/ali_* 2>/dev/null
-#     fi
-#
-#     # clean up files
-#     rm $working_dir/ali_* 2>/dev/null
-# fi
 if [ $stage -le -2 ]; then
-    echo "---map the alignments to $string---"
+    echo "---Map the alignments to $string---"
     # get phoneme based alignments
     for dataset in train test dev; do
         if [[ "$dataset" == "train" ]]; then
-            echo "here"
-            $cmd --num-threads 4 JOB=1:$nj $working_dir/log/${string}_based_alignments_${dataset}_JOB\
-                ali-to$string --per-frame $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|"\
-                    "ark,t:|gzip -c >$working_dir/ali_${string}_${dataset}.JOB.gz"
+            $cmd JOB=1:$nj $working_dir/log/${string}_based_alignments_${dataset}_JOB \
+                ali-to-$string --per-frame $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|" \
+                    "ark,t:|gzip -c >$working_dir/ali_${string}_${dataset}.JOB.gz" || exit 1;
         else
-            $cmd --num-threads 4 JOB=1:$njd $working_dir/log/${string}_based_alignments_${dataset}_JOB\
-                ali-to$string --per-frame $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|"\
-                    "ark,t:|gzip -c >$working_dir/ali_${string}_${dataset}.JOB.gz"
+            $cmd JOB=1:$njd $working_dir/log/${string}_based_alignments_${dataset}_JOB \
+                ali-to-$string --per-frame $mdl "ark,t:gunzip -c $working_dir/ali_conv_${dataset}.JOB.gz|" \
+                    "ark,t:|gzip -c >$working_dir/ali_${string}_${dataset}.JOB.gz" || exit 1;
         fi
         # create one big alignment file
         gunzip -c $working_dir/ali_${string}_${dataset}.*> $working_dir/alignments/all_ali_${string}_${dataset}
@@ -175,29 +128,39 @@ if [ $stage -le -2 ]; then
 
     # rm $working_dir/ali_* 2>/dev/null
 fi
-exit 1;
 
 # concat the features with the labels (alignments)
 # we also create the features for training because we don't have alignments for
 # all the training data
 if [ $stage -le -1 ]; then
-    echo "---filter the features and merge them with the labels ---"
+    echo "---Filter the features and merge them with the labels ---"
     for dataset in train test dev; do
-        $working_dir/tmp_${dataset}
+        mkdir -p $working_dir/tmp_${dataset}
         if [[ "$dataset" == "train" ]]; then
-            python $framework_path/KaldiHelper/KaldiMiscHelper.py --nj $nj $data $working_dir/alignments/all_ali_pdf_${dataset}r $working_dir/tmp_${dataset}
+            python $framework_path/KaldiHelper/KaldiMiscHelper.py --nj $nj $data $working_dir/alignments/all_ali_${string}_${dataset} $working_dir/tmp_${dataset} || exit 1;
         else
-            python $framework_path/KaldiHelper/KaldiMiscHelper.py --nj $njd $data $working_dir/alignments/all_ali_pdf_${dataset}r $working_dir/tmp_${dataset}
+            python $framework_path/KaldiHelper/KaldiMiscHelper.py --nj $njd $dataset $working_dir/alignments/all_ali_${string}_${dataset} $working_dir/tmp_${dataset} || exit 1;
         fi
+        rm $working_dir/tmp_${dataset}/features_* 2>/dev/null
     done
-    python $framework_path/KaldiHelper/KaldiMiscHelper.py --nj $nj $data $ali_dir $working_dir/tmp
-    # # remove some files
-    rm $working_dir/tmp/features_* 2>/dev/null
 fi
 
+
 if [ $stage -le 0 ]; then
-    echo "---creating TFRecords files for training in tensorflow---"
-    python $framework_path/KaldiHelper/MiscHelper.py --nj $nj --state-based $state_based $framework_path/stats.mat $working_dir/tmp $working_dir/tf_data/train
+    echo "---Creating TFRecords files for training in tensorflow---"
+    for dataset in train test dev; do
+        if [[ "$dataset" == "train" ]]; then
+            python $framework_path/KaldiHelper/MiscHelper.py --nj $nj --state-based $state_based $framework_path/stats.mat \
+                $working_dir/tmp_${dataset} $working_dir/tf_data/${dataset} || exit 1;
+        else
+            python $framework_path/KaldiHelper/MiscHelper.py --nj $njd --state-based $state_based $framework_path/stats.mat \
+                $working_dir/tmp_${dataset} $working_dir/tf_data/${dataset} || exit 1;
+        fi
+    done
 fi
+
+echo "---Cleaning up---"
+rm $working_dir/ali_*
+rm -R $working_dir/alignments $working_dir/tmp_*
 
 exit 0
