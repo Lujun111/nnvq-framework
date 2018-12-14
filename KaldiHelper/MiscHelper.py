d7e8f0bc64f20145064de3e943ed8ce92accbf2f
Tobias Watzel <tobias.watzel@tum.de>
2018-12-14 16:41:33 +0100

    changed Helper class

--- KaldiHelper/MiscHelper.py
+++ KaldiHelper/MiscHelper.py
@@ -399,6 +399,6 @@ def main(arguments):
     print('Created TFRecords')
 
 if __name__ == "__main__":
-    misc = Misc(35, True, 0, False)
-    misc.test_mmi()
-    # sys.exit(main(sys.argv[1:]))
+    # misc = Misc(35, True, 0, False)
+    # misc.test_mmi()
+    sys.exit(main(sys.argv[1:]))
